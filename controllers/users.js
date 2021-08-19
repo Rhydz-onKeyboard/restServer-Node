b583710315617fd3a60cb7ea667a9e0bf12038bd
aldiaza <alr.diaza@gmail.com>
2021-08-19 12:02:37 -0400

    backen basico restserver

---
+++ controllers/users.js
@@ -0,0 +1,42 @@
+const { response, request } = require('express');
+
+const usersGet = (req = request, res = response) => {
+    const query = req.query;
+    res.json({
+        msg: 'get API - controller',
+        query
+    })
+};
+const usersPost = (req, res = response) => {
+    const { nombre, edad } = req.body;
+
+    res.json({
+        msg: 'post API - controller',
+        nombre,
+        edad
+    })
+};
+const usersPut = (req, res = response) => {
+    const { id } = req.params;
+    res.json({
+        msg: 'put API - controller',
+        id
+    })
+};
+const usersPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - controller'
+    })
+};
+const usersDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - controller'
+    })
+}
+module.exports = {
+    usersGet,
+    usersPost,
+    usersPut,
+    usersPatch,
+    usersDelete
+}
