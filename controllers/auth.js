7955efd74a181f8fee2547f4401320e32219136d
aldiaza <alr.diaza@gmail.com>
2022-06-08 10:13:25 -0400

    google sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,51 +1,64 @@
-const bcrypt = require("bcryptjs");
-const { response, request } = require("express");
+const bcrypt = require('bcryptjs');
+const { response, request } = require('express');
 const Usuario = require('../models/user');
 const jwt = require('../helpers/generarJwt');
-const { StatusCodes:code } = require('http-status-codes');
-
+const { StatusCodes: code } = require('http-status-codes');
+const google = require('../helpers/googleVerify');
+const { json } = require('express/lib/response');
 
 module.exports = {
-    login: async( req = request, res = response) => {
-
+    login: async (req = request, res = response) => {
         const { correo, pass } = req.body;
-        
 
         try {
             // Verificar si el email existe
             const usuario = await Usuario.findOne({ correo });
             if (!usuario) {
                 return res.status(code.BAD_REQUEST).json({
-                    msg: 'Usuario / Password no son correctos - correo'
+                    msg: 'Usuario / Password no son correctos - correo',
                 });
-            };
+            }
             // Si el usuario esta activo
             if (!usuario) {
                 return res.status(code.BAD_REQUEST).json({
-                    msg: 'Usuario / Password no son correctos - estado: false'
+                    msg: 'Usuario / Password no son correctos - estado: false',
                 });
-            };
+            }
             // Verificar la contrasena
-            const validPass = bcrypt.compareSync( pass, usuario.pass);
+            const validPass = bcrypt.compareSync(pass, usuario.pass);
             if (!validPass) {
                 return res.status(code.BAD_REQUEST).json({
-                    msg: 'Usuario / Password no son correctos - password'
+                    msg: 'Usuario / Password no son correctos - password',
                 });
-            };
+            }
             // Generar el Jwt
 
-            const token = await jwt.generar( usuario.id )
+            const token = await jwt.generar(usuario.id);
             res.json({
                 usuario,
-                token
-            })
+                token,
+            });
         } catch (error) {
             console.log(error);
             return res.status(code.INTERNAL_SERVER_ERROR).json({
-                msg: 'Hable con el administrador'
-            })
+                msg: 'Hable con el administrador',
+            });
         }
-
-
-    }
-}
+    },
+    googleSignIn: async (req = request, res = response) => {
+        const { id_token } = req.body;
+        try {
+            const googleUser = await google.verify(id_token);
+            console.log(googleUser);
+            res.status(code.OK).json({
+                msg: 'Todo bien',
+                id_token,
+            });
+        } catch (error) {
+            res.status(code.BAD_REQUEST).json({
+                ok: false,
+                msg: 'El token no se pudo verificar',
+            });
+        }
+    },
+};
--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,23 @@ const validarCampos = require('../middlewares/validarCampos');
 
 const router = Router();
 
-router.route('/login')
-    .post([
-        check('correo', 'El correo es obligatorio').isEmail(),
-        check('pass', 'La contrasena es obligatoria').not().isEmpty(),
-        validarCampos.validate
-    ], authControllers.login);
-
+router
+    .route('/login')
+    .post(
+        [
+            check('correo', 'El correo es obligatorio').isEmail(),
+            check('pass', 'La contrasena es obligatoria').not().isEmpty(),
+            validarCampos.validate,
+        ],
+        authControllers.login
+    );
+router
+    .route('/google')
+    .post(
+        [
+            check('id_token', 'id_token es necesario').notEmpty(),
+            validarCampos.validate,
+        ],
+        authControllers.googleSignIn
+    );
 module.exports = router;
